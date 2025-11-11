1231b29af4f044e7d899d643e000dae231140ac5
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-11-11 15:48:44 -0500

    Added comments for what app needs to do

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+//Get date
+//Get user temperature, weather description, humidity, & wind speed
+//Change user temperature from fahreneheit to celsius
+//Get user's current location - Add button for it
+//Get user's forecast - Add a div for it
