bcbafeb0073f7618541c133e465a010fa88268db
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-11-13 12:50:23 -0500

    Created function to display current date

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-//Get date
 //Get user temperature, weather description, humidity, & wind speed
 //Change user temperature from fahreneheit to celsius
 //Get user's current location - Add button for it
 //Get user's forecast - Add a div for it
+
+//DATE
+function getDate(date) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[date.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let month = months[date.getMonth()];
+
+  let currentDate = date.getDate();
+
+  return `${day}, ${month} ${currentDate}`;
+}
+
+let currentDate = document.querySelector("#date");
+let now = new Date();
+
+currentDate.innerHTML = getDate(now);
