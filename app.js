5d03d9379792eb85971fa2f8e492cf1c1c983ce8
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-11-13 13:01:04 -0500

    Created functions to handle user input and call weather API

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //Get user's current location - Add button for it
 //Get user's forecast - Add a div for it
 
-//DATE
+//GET DATE
 function getDate(date) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -35,3 +35,30 @@ let currentDate = document.querySelector("#date");
 let now = new Date();
 
 currentDate.innerHTML = getDate(now);
+
+//CURRENT WEATHER
+function displayCurrentWeather(response) {
+  console.log(response);
+}
+
+//API CALLED
+function getWeather(city) {
+  let apiKey = `1bac80fa0c32ft537387a483f19bf3fo`;
+  let apiUrlBase = `https://api.shecodes.io/weather/v1/`;
+
+  let currentWeather = `${apiUrlBase}current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(currentWeather).then(displayCurrentWeather);
+}
+
+//USER SUBMITS FORM
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let userCityInput = document.querySelector("#user-city-input");
+
+  getWeather(userCityInput.value);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
